1a43b7305d64276160783789f3f7e347b51785e3
Maria Jose Salinas <mariasalinas713@gmail.com>
2026-09-03 15:30:05 -0600

    migraciones

--- Biblioteca.Application/Interfaces/ILibroRepository.cs
+++
@@ -1,4 +0,0 @@
-// ARCHIVO OBSOLETO: esta interfaz se movio a
-// Biblioteca.Application/Interfaces/Repositories/ILibroRepository.cs
-// No declara ningun tipo a proposito (para no chocar ni ocultar la version real).
-// No pudo eliminarse desde este entorno; es seguro borrarlo manualmente.

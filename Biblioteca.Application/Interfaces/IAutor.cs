cadbb800f91c51b1256789c02c01c6266316bef9
Maria Jose Salinas <mariasalinas713@gmail.com>
2026-09-03 14:46:35 -0600

    Corrige bug de build: namespacea stubs obsoletos que ocultaban IAutorRepository real

--- Biblioteca.Application/Interfaces/IAutor.cs
+++
@@ -1,8 +0,0 @@
-using System.Globalization;
-using System.Text.RegularExpressions;
-
-public interface IAutor
-{
-    
-
-}
---
+++ Biblioteca.Application/Interfaces/Repositories/IAutorRepository.cs
@@ -0,0 +1,9 @@
+// ARCHIVO OBSOLETO: reemplazado por Biblioteca.Application/Interfaces/IAutorRepository.cs
+// Se conserva (sin poder eliminarse desde este entorno) y se ubica en un namespace propio
+// para que NO choque ni oculte la interfaz real. Seguro de borrar manualmente.
+namespace Biblioteca.Application.Interfaces.Repositories.Obsoleto;
+
+public interface IAutorRepository
+{
+
+}
